9ae1ebc616d01f99e27aa0e054b0118fbe9c6cf4
Stefano <bertonestefano3@gmail.com>
2025-11-12 18:21:03 -0300

    v1.0 menú principal

---
+++ inventario.py
@@ -0,0 +1,26 @@
+print('\nGESTOR DE INVENTARIO\n')
+print('Menú principal\n')
+print('1. Agregar producto')
+print('2. Mostrar producto')
+print('3. Modificar producto')
+print('4. Eliminar producto')
+print('5. Salir\n')
+
+eleccion = int(input('Ingrese el número de la opción deseada: '))
+
+while True:
+    if eleccion == 1:
+        print('\nOpción no declarada.\n')
+        eleccion = int(input('Elige una opción del menú: '))
+    elif eleccion == 2:
+        print('\nOpción no declarada.\n')
+        eleccion = int(input('Elige una opción del menú: '))
+    elif eleccion == 3:
+        print('\nOpción no declarada.\n')
+        eleccion = int(input('Elige una opción del menú: '))
+    elif eleccion == 4:
+        print('\nOpción no declarada.\n')
+        eleccion = int(input('Elige una opción del menú: '))
+    elif eleccion == 5:
+        print('\nHasta luego.\n')
+        break
