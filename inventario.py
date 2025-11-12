ff5518376ee78f0d6f252744726dc79e10fcb003
Stefano <bertonestefano3@gmail.com>
2025-11-12 19:18:08 -0300

    v1.2 implementación de la función mostrar producto

--- inventario.py
+++ inventario.py
@@ -3,6 +3,7 @@
 
 print('\nGESTOR DE INVENTARIO\n')
 
+
 def mostrar_menu():
     print('Menú principal\n')
     print('1. Agregar producto')
@@ -11,16 +12,17 @@ def mostrar_menu():
     print('4. Eliminar producto')
     print('5. Salir\n')
 
+
 inventario = []
 
 while True:
     mostrar_menu()
     eleccion = int(input('Ingrese el número de la opción deseada: '))
-    print() 
+    print()
 
     if eleccion == 1:
-        print('Agregando producto: \n')
-        
+        print('Agregando producto:\n')
+
         nombre = input('Ingrese el nombre del producto: ')
         precio = float(input('Ingrese el precio del producto: '))
         cantidad = int(input('Ingrese la cantidad del producto: '))
@@ -29,19 +31,29 @@ while True:
         inventario.append(producto)
 
         print('\nProducto añadido exitosamente.\n')
-        print()
 
     elif eleccion == 2:
-        print('\nOpción no declarada.\n')
-        print()
+        if len(inventario) == 0:
+            print('No hay productos registrados.\n')
+            continue
+
+        producto_buscado = input('Ingrese el nombre del producto que desea buscar: ')
+        encontrado = False
+
+        for i in inventario:
+            if producto_buscado.lower() == i['nombre'].lower():
+                print(f"\nNombre: {i['nombre']}\nPrecio: ${i['precio']}\nCantidad: {i['cantidad']}\n")
+                encontrado = True
+                break
+
+        if not encontrado:
+            print('\nEl producto no existe en el inventario.\n')
 
     elif eleccion == 3:
         print('\nOpción no declarada.\n')
-        print()
 
     elif eleccion == 4:
         print('\nOpción no declarada.\n')
-        print()
 
     elif eleccion == 5:
         print('\nHasta luego.\n')
