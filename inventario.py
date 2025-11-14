a05d6f1e0b008fa63e78becbf27e330c77159af9
Stefano <bertonestefano3@gmail.com>
2025-11-13 22:38:00 -0300

    v1.3 Mejora en el agregado y mostrado de producto

--- inventario.py
+++ inventario.py
@@ -3,7 +3,6 @@
 
 print('\nGESTOR DE INVENTARIO\n')
 
-
 def mostrar_menu():
     print('Menú principal\n')
     print('1. Agregar producto')
@@ -20,34 +19,40 @@ while True:
     eleccion = int(input('Ingrese el número de la opción deseada: '))
     print()
 
+    # agregar producto
     if eleccion == 1:
         print('Agregando producto:\n')
-
-        nombre = input('Ingrese el nombre del producto: ')
+        
+        nombre = input('Ingrese el nombre del producto: ').strip().title()
         precio = float(input('Ingrese el precio del producto: '))
         cantidad = int(input('Ingrese la cantidad del producto: '))
+        
+        for i in inventario:
+            if nombre.lower() == i['nombre'].lower():
+                print(f'El producto "{nombre}" ya existe en el inventario')
+                
+                sumar = input('¿Desea sumar la cantidad ingresada al producto ya existente? (si/no): ')
+                
+                if sumar.lower() in ('si', 'sí'):
+                    i['cantidad'] += cantidad
+                    print('La cantidad se sumó efectivamente.\n')
+                else:
+                    print('No se sumó la cantidad y no se agregó un producto nuevo.\n')
+                break
+        else:
+            producto = {'nombre': nombre, 'precio': precio, 'cantidad': cantidad}
+            inventario.append(producto)
+            print('Producto añadido exitosamente.\n')
 
-        producto = {'nombre': nombre, 'precio': precio, 'cantidad': cantidad}
-        inventario.append(producto)
-
-        print('\nProducto añadido exitosamente.\n')
-
+    # mostrar producto
     elif eleccion == 2:
         if len(inventario) == 0:
             print('No hay productos registrados.\n')
             continue
 
-        producto_buscado = input('Ingrese el nombre del producto que desea buscar: ')
-        encontrado = False
-
-        for i in inventario:
-            if producto_buscado.lower() == i['nombre'].lower():
-                print(f"\nNombre: {i['nombre']}\nPrecio: ${i['precio']}\nCantidad: {i['cantidad']}\n")
-                encontrado = True
-                break
-
-        if not encontrado:
-            print('\nEl producto no existe en el inventario.\n')
+        for p in inventario:
+            print(f"{p['nombre']} | Precio: ${p['precio']} | Cantidad: {p['cantidad']}")
+        print()
 
     elif eleccion == 3:
         print('\nOpción no declarada.\n')
